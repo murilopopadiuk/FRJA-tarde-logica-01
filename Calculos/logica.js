55daaacc610b6eb6e4da534d4a59b0c617f1f06b
murilopopadiuk <murilo.popadiuk@aluno.senai.br>
2025-10-02 16:52:20 -0300

    Add files via upload

--- Calculos/logica.js
+++ Calculos/logica.js
@@ -10,9 +10,26 @@ resposta.textContent = numero + numero2
 
 function sub(){
     
-    var numero = document.getElementById("s1").valueAsNumber
-    var numero2 = document.getElementById("s2").valueAsNumber
+    var numero = parseFloat(document.getElementById("s1").value)
+    var numero2 = parseFloat(document.getElementById("s2").value)
     var resposta = document.getElementById("respSub")
 resposta.textContent = numero - numero2
 }
 
+function multi(){
+    var number1 = parseFloat(document.getElementById("n5").value)
+    var number2 = parseFloat(document.getElementById("n6").value)
+    document.getElementById("respMulti").textContent = number1 * number2
+
+}
+function div(){
+    var a = parseFloat(document.getElementById("n7").value)
+     var b = parseFloat(document.getElementById("n8").value)
+     var resp = document.getElementById("respDiv")
+
+     if(b !== 0){
+        resp.innerHTML =(a / b).toFixed(2)
+     }else{
+        resp.innerHTML = "nao se divide por 0"
+     }
+}
